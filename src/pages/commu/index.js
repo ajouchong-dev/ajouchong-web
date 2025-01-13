30c5066dda874d5a5231a5565210acc1f0fd0855
y.tnwjd <ysjsw@ajou.ac.kr>
2025-01-13 17:53:56 +0900

    chore: change commu link

--- src/pages/commu/index.js
+++ src/pages/commu/index.js
@@ -7,7 +7,7 @@ const Commu = () => {
             <div className="contextTitle">통합 소통 창구</div>
             <hr className="titleSeparator"/>
             <ul className="proceeding-container">
-            <li><a href="https://forms.gle/ynoZGkmY8uQdbcoc7"> > 통합 소통 창구 바로가기 </a></li>
+            <li><a href="https://forms.gle/m9QkFtVWfTvLi9Sp8"> > 통합 소통 창구 바로가기 </a></li>
             </ul>
         </div>
     );
