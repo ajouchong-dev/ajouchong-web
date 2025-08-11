800dfa9c0af31c67da693b5d19a42c1cc7ae9002
y.tnwjd <ysjsw@ajou.ac.kr>
2025-08-11 23:43:26 +0900

    chore: change acetia gods to goods

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -73,7 +73,7 @@ const Header = () => {
     const isResourcesActive = ['/resources/bylaws',  '/resources/proceeding','/resources/audit'].includes(location.pathname);
     const isCommunicationActive = ['/communication/qna', '/communication/require'].includes(location.pathname);
     const isWelfareActive = ['/welfare/promotion', '/welfare/rental'].includes(location.pathname);
-    const isAcentiaActive = ['/acentia/intro', '/acentia/gods', '/acentia/history'].includes(location.pathname);
+    const isAcentiaActive = ['/acentia/intro', '/acentia/goods', '/acentia/history'].includes(location.pathname);
 
     return (
         <div>
@@ -193,7 +193,7 @@ const Header = () => {
                                 {dropdown === 'acentia' && (
                                     <ul className="dropdown-container">
                                         <li><a href="/acentia/intro">ACENTIA 소개</a></li>
-                                        <li><a href="/acentia/gods">ACENTIA 굿즈</a></li>
+                                        <li><a href="/Acentia/goods">ACENTIA 굿즈</a></li>
                                         <li><a href="/acentia/history">역대 ACENTIA</a></li>
                                     </ul>
                                 )}
@@ -268,7 +268,7 @@ const Header = () => {
                                 {dropdown === 'acentia' && (
                                     <ul className="dropdown">
                                         <li><a href="/acentia/intro">ACENTIA 소개</a></li>
-                                        <li><a href="/acentia/gods">ACENTIA 굿즈</a></li>
+                                        <li><a href="/Acentia/goods">ACENTIA 굿즈</a></li>
                                         <li><a href="/acentia/history">역대 ACENTIA</a></li>
                                     </ul>
                                 )}
---
+++ src/pages/Acentia/goods.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./gods.css";
+
+const Gods = () => {
+    return (
+        <div className="context">
+            <div className="contextTitle">ACENTIA 굿즈 소개</div>
+            <hr className="titleSeparator"/>
+            <div className="gods">
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Gods;
